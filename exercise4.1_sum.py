e6e22009af5ef592223584f219ac2c9569a1ca90
SZJhacker <shenzijie2013@163.com>
2018-08-31 16:47:15 +0800

    "function of mysum"

--- exercise4.1_sum.py
+++ exercise4.1_sum.py
@@ -1,7 +1,7 @@
-def sum(li):
+def mysum(li):
     if li == []:
         return 0
     return li[0] + sum(li[1:])
 
 if __name__ == "__main__":
-    print(sum([1, 3, 5, 7, 9, 11]))
+    print(mysum([1, 3, 5, 7, 9, 11]))
